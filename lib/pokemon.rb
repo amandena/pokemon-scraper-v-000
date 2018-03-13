4a78a82d07e536b55be989eba3709cad78b3421d
amanda johnson <manda.panda91@yahoo.com>
2018-03-13 12:22:09 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
+    pokemon.new(id, name, type, db)
+    pokemon
+  end
 end
